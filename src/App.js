faba53e6eaf0a40bc218c0bc2ff7037c38ff9a1e
= <=>
2022-06-17 21:31:03 +0430

    Main section and responsive sections creaetd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <Main />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,118 @@
 import React from 'react'
 import {Fragment} from 'react'
-import {Disclouser, Menu, Transition} from '@headlessio/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import {Disclosure} from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import logo from '../assets/images/logo.svg'
 
 const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
+    
+    { name: 'Features', href: '#', current: false },
+    { name: 'Company', href: '#', current: false },
+    { name: 'Careers', href: '#', current: false },
+    { name: 'About', href: '#', current: false },
 ]
+
+const secondNav = [
+    {name: 'Login', href: '#', current: false},
+    {name: 'Register', href: '#', current: false},
+
+]
+
+function classNames(...classes){
+    return classes.filter(Boolean).join(' ')
+}
 
 function Navbar() {
     return (
 
+        <Disclosure as="nav" className="shadow-xl py-3">
+            {({ open }) => (
+                <>
+                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex items-center justify-between h-16">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        {/* Mobile menu button*/}
+                        <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                            <XIcon className="block h-6 w-6" aria-hidden="true" />
+                        ) : (
+                            <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                        )}
+                        </Disclosure.Button>
+                    </div>
+                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex-shrink-0 flex items-center">
+                        <img
+                            className="block lg:hidden h-8 w-auto"
+                            src={logo}
+                            alt="Workflow"
+                        />
+                        <img
+                            className="hidden lg:block h-8 w-auto mr-10"
+                            src={logo}
+                            alt="Workflow"
+                        />
+                        </div>
+                        <div className="hidden sm:block sm:ml-6">
+                        <div className="flex space-x-4">
+                            {navigation.map((item) => (
+                            <a
+                                key={item.name}
+                                href={item.href}
+                                className={classNames(
+                                item.current ? 'text-gray-900' : 'hover:bg-gray-700 hover:text-white',
+                                'px-3 py-2 rounded-md text-sm font-medium'
+                                )}
+                                aria-current={item.current ? 'page' : undefined}
+                            >
+                                {item.name}
+                            </a>
+                            ))}
+                        </div>
+                        </div>
+                    </div>
+                        <div>
+                            <div className="flex space-x-4">
+                                {secondNav.map((item) => (
+                                <a
+                                    key={item.name}
+                                    href={item.href}
+                                    className={classNames(
+                                    item.current ? 'text-gray-900' : 'hover:bg-gray-700 hover:text-white',
+                                    'px-3 py-2 rounded-md text-sm font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <Disclosure.Panel className="sm:hidden">
+                    <div className="px-2 pt-2 pb-3 space-y-1">
+                    {navigation.map((item) => (
+                        <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className={classNames(
+                            item.current ? 'bg-white text-black' : 'text-black hover:bg-gray-700 hover:text-white',
+                            'block px-3 py-2 rounded-md text-base font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                        >
+                        {item.name}
+                        </Disclosure.Button>
+                    ))}
+                    </div>
+                </Disclosure.Panel>
+                </>
+            )}
+    </Disclosure>
 
     )
 }
